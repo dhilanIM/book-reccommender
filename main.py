cc0f065bd89e5b7192cd0bae274c9252170096aa
dhilanIM <cls_dhilan@hotmail.com>
2025-08-04 12:00:16 -0600

    Added new files for deployment in railway

--- main.py
+++ main.py
@@ -96,5 +96,5 @@ with gr.Blocks(theme=gr.themes.Glass()) as dashboard:
     submit_button.click(fn = recommed_book, inputs= [user_query, category_dropdown, tone_dropdown], outputs= output)
     
 if __name__ == "__main__":
-    dashboard.launch()
+    dashboard.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
         
